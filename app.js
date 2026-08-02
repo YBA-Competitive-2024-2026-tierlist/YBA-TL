6a1fcfa487515bcd51b5baceb783b9641ec1e17b
Shayraim <159888407+Shayraim@users.noreply.github.com>
2026-08-02 17:47:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -426,8 +426,8 @@ function App() {
             React.createElement('span', { className: 'creator-name' }, 'Shayraim')
           ),
           React.createElement('div', { className: 'creator' },
-            React.createElement(ProfileCard, { src: 'image/b664188f61102fcf384c35504edd570c.jpg', name: 'Havoconslaught' }),
-            React.createElement('span', { className: 'creator-name' }, 'Havoconslaught')
+            React.createElement(ProfileCard, { src: 'image/b664188f61102fcf384c35504edd570c.jpg', name: 'NecroticHollow' }),
+            React.createElement('span', { className: 'creator-name' }, 'NecroticHollow')
           )
         ),
 
